cfb8333b075e5e8ed8526a51dafb4e664bf5744e
Langeti <example@gmail.com>
2022-03-25 19:23:11 +0200

    practice part 2

--- js/main.js
+++ js/main.js
@@ -7,8 +7,26 @@ let personalMovieDB = {
    privat: false
 };
 
-const a = prompt('Який фільм?',''),
-      b = prompt(`Оцінка ${a}`,'');
+for(let i = 0; i < 2; i++){
+   const a = prompt('Який фільм?',''),
+         b = prompt(`Оцінка ${a}`,'');
+   if( a != null && a != '' && a.length<50 && b != null && b != '' && b.length<50){
+      personalMovieDB.movies[a] = +b;
+      console.log('done');
+   } else {
+      console.log('no done');
+      i--;
+   }
+}
 
-personalMovieDB.movies[a] = +b;
+if(personalMovieDB.count < 10){
+   console.log('Ви подивились доволі мало фільмів');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+   console.log('Ви класичний глядач');
+} else if (personalMovieDB.count > 30){
+   console.log('Ви кіноман');
+} else {
+   console.log('Щось пішло не так!');
+}
+
 console.log(personalMovieDB);
