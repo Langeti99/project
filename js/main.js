c77d5471f6193ba156f17b4f464b8a66f708d360
Langeti <example@gmail.com>
2022-03-25 15:25:31 +0200

    first app

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-let incr = 10,
-    decr = 10;
+const numberOfFilms = +prompt('Скільки фільмів ти вже подивився?', '');
+let personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
 
-console.log(--incr);
-console.log(++decr);
+const a = prompt('Який фільм?',''),
+      b = prompt(`Оцінка ${a}`,'');
+
+personalMovieDB.movies[a] = +b;
+console.log(personalMovieDB);
