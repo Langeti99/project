4fdf343682b8fd8dc7bb66a398dcb65673ecb793
Langeti <example@gmail.com>
2022-03-28 23:46:50 +0300

    lesson 27

--- js/main.js
+++ js/main.js
@@ -47,6 +47,17 @@ const personalMovieDB = {
    writeYourGenres: function(){
       for(let i = 0; i < 3; i++){
         personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`,'');
+        if(personalMovieDB.genres[i] == null || personalMovieDB.genres[i] == ''){
+           i--;
+        }
+      }
+      personalMovieDB.genres.forEach((item, i) => console.log(`Любимый жанр №${i+1} - это ${item}`));
+   },
+   toggleVisibleMyDB: function(){
+      if(!personalMovieDB.privat){
+         personalMovieDB.privat = true;
+      } else {
+         personalMovieDB.privat = false;
       }
    }
 };
